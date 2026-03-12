6df3d4e8bffddde47fbb6a0b7a37032a76fe6f28
Sahil Shaw <shaws2857@gmail.com>
2026-03-12 21:30:43 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-red-600">Hello World</h1>
-    </div>
-  );
-};
+const App = () => {};
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
